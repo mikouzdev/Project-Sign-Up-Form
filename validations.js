c0930ece8e5d17ce0ea3be4ea9953d1bb8be37b7
burabotti <burabotti@gmail.com>
2023-05-09 20:29:17 +0300

    odin project

---
+++ validations.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("signUpForm");
+const password1Input = document.querySelector("#pass1");
+const password2Input = document.querySelector("#pass2");
+let doesMatch;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!doesMatch) {
+    alert("Passwords do not match");
+    password1Input.classList.add("error");
+    password2Input.classList.add("error");
+    return;
+  }
+
+  // If the passwords match, submit the form
+  form.submit();
+});
+
+password2Input.addEventListener("input", () => {
+  if (password1Input.value !== "") {
+    if (password1Input.value !== password2Input.value) {
+      doesMatch = false;
+    } else {
+      doesMatch = true;
+    }
+  }
+});
